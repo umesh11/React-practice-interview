85f38ba5fb646a7a8dd2f871c8127044eea04759
umeshkumar <umesh11sh@gmail.com>
2024-01-16 00:45:07 +0530

    changes related to file structure

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,29 @@
-import {Button} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = (props) => {
+    const [data, setData] = useState([]);
+
+
+    function getDummyData() {
+        axios.get('https://jsonplaceholder.typicode.com/todos/1').then((res) => {
+            if(!res.status == 200) return;
+            setData(res.data);
+        })
+    }
+
+    useEffect(() => {
+       getDummyData();
+    }, [])
+
+    console.log({data})
 
     return (
         <>
-        <Button>
+        <h5>{data.title}</h5>
+        <button>
          Click Me
-        </Button>
+        </button>
         </>
     )
 }
