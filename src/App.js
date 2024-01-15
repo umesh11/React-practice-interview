cfab39da774908181ea7819155e086e6adaacd6f
umeshkumar <umesh11sh@gmail.com>
2024-01-15 22:07:09 +0530

    react practicing day1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       <h5>This is react app</h5>
+       <Home />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import {Button} from 'react-router-dom';
+
+const Home = (props) => {
+
+    return (
+        <>
+        <Button>
+         Click Me
+        </Button>
+        </>
+    )
+}
+
+export default Home;
