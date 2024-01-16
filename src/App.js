20257669e1287dbf3b0c6c8745c60d5425efcaf5
umeshkumar <umesh11sh@gmail.com>
2024-01-16 11:10:07 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
        <h5>This is react app</h5>
        <Home />
       </header>
